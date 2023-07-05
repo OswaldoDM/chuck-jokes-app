e768db8d0613bf06089d28f1c4cf0cb404e78165
Oswaldo <oswaldoe1992@gmail.com>
2023-07-05 03:47:30 -0400

    new

---
+++ src/ChuckJoke.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+
+import * as API from '../src/services/getChuck'
+
+import { Logo } from "./components/Logo"
+
+
+export function ChuckJoke() {
+
+    const { category } = useParams()
+
+    const [jokes , setJokes] = useState( {} );
+    const { categoryStr, joke } = jokes
+    
+    
+
+    useEffect(() => {
+      
+        API.getJokeByCategory( category )
+            .then( setJokes )
+            .catch( console.error )
+      
+    }, [ category ]);
+    
+    function handleClick(){
+        API.getJokeByCategory( category )
+            .then( setJokes )
+            .catch( console.error )
+    };   
+    
+
+  return (
+    <>
+    <div className="container jokes">
+
+        <Logo />
+
+        {
+            ( !joke ) ? 
+
+            
+            <div className="spinner-3"></div>
+        
+            :
+            <div>
+                <h1 className="category font-04B">{categoryStr}</h1>
+                <p className="joke">{joke}</p>              
+                <button onClick={handleClick}> Next </button>
+            </div>
+        }        
+
+        <div>
+            <Link to='/'>
+                <h1 className="back font-04B">
+                Back to categories
+                </h1>
+            </Link>
+        </div>
+
+    </div>    
+    </>
+  )
+}
---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/chucklogo.png'
+
+export function Logo() {
+
+  return (
+    <div className="logo-container">
+        <img src={logo} alt="chuck-logo" loading="lazy" />        
+    </div>
+  )
+}
