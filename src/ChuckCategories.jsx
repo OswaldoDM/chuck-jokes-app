f52a379dfe385bfdca38d620032b1bb1a6589cf1
Oswaldo <oswaldoe1992@gmail.com>
2023-07-05 18:10:33 -0400

    no logo

--- src/ChuckCategories.jsx
+++ src/ChuckCategories.jsx
@@ -24,7 +24,7 @@ export function ChuckCategories() {
     <section>
     <div className="container app">
 
-      <Logo />
+      {/* <Logo /> */}
 
       <h1 className="font-04B">Choose a Category</h1>
       
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,10 @@
-import logo from '../images/chucklogo.png'
 
-export function Logo() {
 
-  return (
-    <div className="logo-container">
-        <img src={logo} alt="chuck-logo" loading="lazy" />        
-    </div>
-  )
-}
+// export function Logo() {
+
+//   return (
+//     <div className="logo-container">
+//         <img src={logo} alt="chuck-logo" loading="lazy" />        
+//     </div>
+//   )
+// }
